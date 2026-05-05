1705ab28116c6000f8f310589dcbf52a34c30a91
cnwang <cnwang406@gmail.com>
2026-05-05 22:58:57 +0800

    Revert "add filtered data preview"
    This reverts commit 7724aaeea0bbca7087256b7c001eb18fc04ca35c.

--- app.py
+++ app.py
@@ -25,9 +25,6 @@ from tabScatter import WEB_ENGINE_AVAILABLE, TabScatterWidget
 
 QT_PLUGIN_PATH = os.path.join(os.path.dirname(PySide6.__file__), 'Qt', 'plugins')
 QT_PLATFORM_PLUGIN_PATH = os.path.join(QT_PLUGIN_PATH, 'platforms')
-print (QT_PLUGIN_PATH)
-print (QT_PLATFORM_PLUGIN_PATH)
-
 APP_NAME = 'p-chart'
 APP_VERSION = 'v2.2.2'
 APP_AUTHOR = 'cnwang'
@@ -188,7 +185,7 @@ class AppMain:
 
         acknowledgeTextEdit = QTextEdit()
         acknowledgeFont = acknowledgeTextEdit.font()
-        acknowledgeFont.setPointSize(12)
+        acknowledgeFont.setPointSize(14)
         acknowledgeTextEdit.setFont(acknowledgeFont)
         acknowledgeTextEdit.setTextColor(QColor('darkblue'))
         acknowledgeTextEdit.setReadOnly(True)
